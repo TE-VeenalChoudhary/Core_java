9ed50e499dfb2274265d988e8c69349c1b9281b7
vc2010 <86232932+vc2010@users.noreply.github.com>
2021-07-01 21:11:30 +0530

    Loops

---
+++ loops/general/LoopingStatements.java
@@ -0,0 +1,16 @@
+package com.te.loops.general;
+
+import java.util.Iterator;
+
+public class LoopingStatements {
+
+	public static void main(String[] args) {
+		for (int i=1; i<=20;i++)
+		{
+			System.out.println(i);
+			
+			i=i*2; // i=i+i;
+		}
+	}
+
+}
